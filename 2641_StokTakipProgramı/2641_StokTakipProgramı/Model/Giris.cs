267c8b486f8e7f06c69fd95834eb2bbe808dde8e
NurESMAA <profilo12Aesma@gmail.com>
2024-12-02 11:35:11 +0300

    Add files via upload

---
+++ 2641_StokTakipProgramı/2641_StokTakipProgramı/Model/Giris.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2641_StokTakipProgramı.Model
+{
+    public class Giris
+    {
+    }
+}
---
+++ 2641_StokTakipProgramı/2641_StokTakipProgramı/Model/Kategori.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2641_StokTakipProgramı.Model
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+
+
+        public string Kategoriadi { get; set; }
+
+        // İlişki : Bir kategorinin birden fazla ürüne sahip olabilir.
+
+        public virtual ICollection<Urun> Urunler { get; set; } = new HashSet<Urun>();
+    }
+}
